41dd6e2bcbd145761b0046e30f997316944264e6
Łukasz Koziorowski <koziorowskilukasz1@gmail.com>
2020-09-01 11:19:03 +0200

    Complete dekstop layout of AccessoriesLayout component

--- src/layouts/AccessoriesLayout.js
+++ src/layouts/AccessoriesLayout.js
@@ -1,7 +1,15 @@
 import React from "react";
 import styles from "./Layout.module.scss";
+import { Link } from "gatsby";
 //paths
 import lampPath from "../../static/images/header-lamp.png";
+import macbookPath from "../../static/images/macbook1.png";
+import walletPath from "../../static/images/black-wallet.png";
+import beltPath from "../../static/images/belt.png";
+import slimWalletPath from "../../static/images/slim-wallet.png";
+import sunglassesPath from "../../static/images/sunglasses.png";
+import sleevePath from "../../static/images/sleeve.png";
+import tableLampPath from "../../static/images/table-lampp.png";
 
 const AccessoriesLayout = () => {
   return (
@@ -18,8 +26,71 @@ const AccessoriesLayout = () => {
         <img className={styles.LayoutHero__img} src={lampPath} alt="lamp" />
       </header>
       <main className={styles.LayoutList}>
+        <figure className={styles["LayoutList__itemFirst"]}>
+          <Link className={styles.LayoutList__imgWrapper}>
+            <img src={macbookPath} alt="macbook case" />
+          </Link>
+          <figcaption className={styles.LayoutList__caption}>
+            Leather macBook Case
+            <strong className={styles.LayoutList__strong}>$179.00</strong>
+          </figcaption>
+        </figure>
         <figure className={styles.LayoutList__item}>
-          <img src="" alt="macbook case" />
+          <Link className={styles.LayoutList__imgWrapper}>
+            <img src={walletPath} alt="wallet" />
+          </Link>
+          <figcaption className={styles.LayoutList__caption}>
+            Black Canvas Wallet
+            <strong className={styles.LayoutList__strong}>$49.00</strong>
+          </figcaption>
+        </figure>
+        <figure className={styles.LayoutList__item}>
+          <Link className={styles.LayoutList__imgWrapper}>
+            <img src={beltPath} alt="belt" />
+          </Link>
+          <figcaption className={styles.LayoutList__caption}>
+            Black Leather Belt
+            <strong className={styles.LayoutList__strong}>$59.00</strong>
+          </figcaption>
+        </figure>
+        <figure className={styles.LayoutList__item}>
+          <Link className={styles.LayoutList__imgWrapper}>
+            <img src={slimWalletPath} alt="slim wallet" />
+          </Link>
+          <figcaption className={styles.LayoutList__caption}>
+            Black Genuine Card Holder
+            <strong className={styles.LayoutList__strong}>$29.00</strong>
+          </figcaption>
+        </figure>
+
+        <figure className={styles.LayoutList__item}>
+          <Link className={styles.LayoutList__imgWrapper}>
+            <img src={sunglassesPath} alt="sunglasses" />
+          </Link>
+          <figcaption className={styles.LayoutList__caption}>
+            Black Ray Ban Glasses
+            <strong className={styles.LayoutList__strong}>$129.00</strong>
+          </figcaption>
+        </figure>
+
+        <figure className={styles.LayoutList__item}>
+          <Link className={styles.LayoutList__imgWrapper}>
+            <img src={sleevePath} alt="iPhone 6 sleeve" />
+          </Link>
+          <figcaption className={styles.LayoutList__caption}>
+            Leather iPhone6 Sleeve
+            <strong className={styles.LayoutList__strong}>$69.00</strong>
+          </figcaption>
+        </figure>
+
+        <figure className={styles.LayoutList__item}>
+          <Link className={styles.LayoutList__imgWrapper}>
+            <img src={tableLampPath} alt="lamp" />
+          </Link>
+          <figcaption className={styles.LayoutList__caption}>
+            Wooden Table Lamp
+            <strong className={styles.LayoutList__strong}>$49.00</strong>
+          </figcaption>
         </figure>
       </main>
     </>
